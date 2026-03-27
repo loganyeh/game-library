bc36838d146ad74d77e3f62f293ff1475dd0bb77
loganyeh <yehlogan@gmail.com>
2026-03-26 23:08:47 -0400

    b4 npm audit fix --force

---
+++ src/Pages/DashboardPage.tsx
@@ -0,0 +1,12 @@
+
+
+function DashboardPage(){
+    
+    return(
+        <>
+            <div className="border-4 border-blue-600 h-screen w-screen"></div>
+        </>
+    )
+}
+
+export default DashboardPage;
