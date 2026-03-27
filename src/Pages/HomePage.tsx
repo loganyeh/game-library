3b09753b1ac67750b9dd57d750c543717128556f
loganyeh <yehlogan@gmail.com>
2026-03-27 17:40:07 -0400

    feat: PageHeader

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -15,10 +15,13 @@ function DashboardPage(){
 
     return(
         <>
-            <div className="border-2 border-green-500 h-auto w-screen">
+            <div className="h-auto w-screen">
 
-                {/* Page Header */}
-                <div className="border-2 border-red-600 h-12 w-full">header</div>
+                {/* Page Header ✅ */}
+                <div className="h-12 w-full flex justify-between items-center bg-red-600">
+                    <div className="border-3 border-white h-6 w-auto ml-4 px-1 flex justify-center items-center text-white text-base font-bold rounded-2xl">Nintendo</div>
+                    <div className="h-auto w-auto mr-4 flex justify-center items-center text-3xl">🇺🇸</div>
+                </div>
 
                 {/* Showcase Slideshow */}
                 <div className="border h-auto w-full">
