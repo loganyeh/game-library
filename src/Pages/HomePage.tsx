7be9e8916d76a0c52cf6bb992640722b47002efa
loganyeh <yehlogan@gmail.com>
2026-03-27 18:46:59 -0400

    feat: Featured Section scroll behavior

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -54,19 +54,21 @@ function DashboardPage(){
                     </div>
 
                     {/* Featured Scroll Tiles */}
-                    <div className="h-[290px] w-full flex justify-start items-center">
+                    <div className="h-[290px] w-auto flex justify-start items-center overflow-x-auto scroll-smooth">
 
-                        {Array.from({length: 1}).map((_, index) => {
-                            return <div key={index} className="border border-gray-300 h-[250px] w-52 mx-4 rounded-lg">
-                                <div className="h-[115px] w-full bg-blue-300 rounded-t-lg"></div>
-                                <div className="h-[135px] w-full flex flex-col justify-between items-center rounded-b-lg">
-                                    <div className="h-11 flex justify-center items-center tracking-wide font-bold text-sm text-gray-700">Play the free demo today!</div>
-                                    <div className="h-auto w-full">
-                                        <span className="h-10 w-auto ml-2 px-2 py-0.5 bg-blue-700 text-white text-[10px] font-medium rounded-xl">Pre-order now</span>
-                                        <div className="h-8 mt-1 flex justify-start items-start"><span className="border-l-2 border-red-600 ml-2 pl-1 text-[10px] text-gray-600">Nintendo Switch</span></div>
+                        {Array.from({length: 10}).map((_, index) => {
+                            return <div key={index} className="border border-gray-300 h-[250px] w-52 mx-4 rounded-lg shrink-0">
+
+                                        <div className="h-[115px] w-full bg-blue-300 rounded-t-lg"></div>
+                                        
+                                        <div className="h-[135px] w-full flex flex-col justify-between items-center rounded-b-lg">
+                                            <div className="h-11 flex justify-center items-center tracking-wide font-bold text-sm text-gray-700">Play the free demo today!</div>
+                                            <div className="h-auto w-full">
+                                                <span className="h-auto w-auto ml-2 px-2 py-0.5 bg-blue-700 text-white text-[10px] font-medium rounded-xl">Pre-order now</span>
+                                                <div className="h-8 w-full mt-1 flex justify-start items-start"><span className="border-l-2 border-red-600 ml-2 pl-1 text-[10px] text-gray-600">Nintendo Switch</span></div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
                         })}
 
                     </div>
