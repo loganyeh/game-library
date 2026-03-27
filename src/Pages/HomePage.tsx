9762dffe9c13f24b26cc05f0b4be4b219eb68caf
loganyeh <yehlogan@gmail.com>
2026-03-27 19:18:41 -0400

    qs.

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -45,7 +45,7 @@ function DashboardPage(){
 
                 </div>
 
-                {/* Featured Block */}
+                {/* Featured Block ✅ */}
                 <div className="border-b border-gray-300 h-[400px] w-full">
                     
                     {/* title */}
@@ -74,12 +74,46 @@ function DashboardPage(){
                     </div>
 
                 </div>
+                {/* PRIOR MORE PRECISE H/W PIXELS */}
 
                 {/* Today! Block */}
-                <div className="h-[500px] w-full">Today! Block</div>
+                <div className="border-b border-gray-300 h-auto w-full">
+                {/* <div className="border-b border-gray-300 h-[500px] w-full"> */}
+
+                    {/* title */}
+                    <div className="h-16 w-full flex justify-start items-end">
+                        <span className="ml-4 mb-2 tracking-wider text-xl text-gray-600 font-bold">Nintendo Today!</span>
+                    </div>
+
+                    {/* nintendo today image */}
+                    <div className="h-auto w-full flex justify-center items-start">
+                        <div className="h-44 w-11/12 rounded-lg">
+                            <div className="h-36 bg-blue-300 rounded-t-lg"></div>
+                            <div className="h-8 bg-red-600 flex justify-center items-center rounded-b-lg">
+                                <div className="h-5 w-5 mr-2 flex justify-center items-center bg-yellow-400 rounded-sm"><i className='bx bx-question-mark text-xl text-white' ></i></div>
+                                <div className="text-white text-sm font-medium tracking-wider">Nintendo Today!</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="h-28 w-full flex justify-center items-center">
+                        <span className="h-auto w-11/12 text-lg font-bold text-gray-700 tracking-wider">Your movie companion: Nintendo Today!</span>
+                    </div>
+
+                   <div className="h-auto w-full flex justify-center items-center">
+                        <div className="h-12 w-11/12 flex justify-center items-center bg-red-600 text-white font-semibold text-base tracking-wider rounded-lg">Learn More</div>
+                    </div> 
+
+                    <div className="h-24 w-full flex justify-center items-start">
+                        <span className="mt-4 pl-4 text-[10px] text-gray-700">
+                            Nintendo Account, compatible smart device and persistent internet connection required. Data charges may apply. Requires iOS 16.0 or later. Requires Android 10.0 or later.
+                        </span>
+                    </div>
+
+                </div>
 
                 {/* Nintendo Switch 2 Block */}
-                <div className="border-2 border-red-600 h-[500px] w-full">Nintendo Switch 2 Block</div>
+                <div className="h-[500px] w-full">Nintendo Switch 2 Block</div>
 
                 {/* Online Store */}
                 <div className="border-2 border-red-600 h-[475px] w-full">Online Store</div>
