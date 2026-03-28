21d9cfe249af372f393763d47ab58f79084494b6
loganyeh <yehlogan@gmail.com>
2026-03-27 21:58:06 -0400

    feat: News Section

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -207,7 +207,7 @@ function DashboardPage(){
                 </div>
 
                 {/* News Block */}
-                <div className="border-2 border-orange-500 h-auto w-full">
+                <div className="h-auto w-full">
 
                     {/* title */}
                     <div className="h-16 w-full flex justify-start items-end">
@@ -215,37 +215,51 @@ function DashboardPage(){
                     </div>
 
                     {/* New Article 1 Big */}
-                    {Array.from({length: 2}).map((_, index) => {
-                        return <div key={index} className="h-auto w-full mb-8 flex flex-col justify-center items-center">
+                    <div className="h-auto w-full">
+                        {Array.from({length: 2}).map((_, index) => {
+                            return <div key={index} className="h-auto w-full mb-8 flex flex-col justify-center items-center">
 
-                                    <div className="h-auto w-11/12 rounded-lg">
-                                        <div className="h-48 bg-blue-300 rounded-lg"></div>
+                                        <div className="h-auto w-11/12 rounded-lg">
+                                            <div className="h-48 bg-blue-300 rounded-lg"></div>
+                                        </div>
+
+                                        <div className="h-11 w-11/12 flex justify-start items-center">
+                                            <div className="mr-1 flex justify-center items-center"><i className='bx bx-plus-medical text-red-600 text-2xl' ></i></div>
+                                            <div className="text-gray-700 font-light text-sm">03/27/26</div>
+                                        </div>
+
+                                        <div className="h-auto w-11/12 text-gray-800 font-semibold text-base tracking-wide line-clamp-2">Get ready for some wondrous new content for Super Mario Bros. Wonder—exclusive to Nintendo Switch 2!</div>
+
+                                        <div className="h-auto w-11/12 mt-2 text-gray-800 line-clamp-2 text-sm font-light">The Super Mario Bros.™ Wonder – Nintendo Switch™ 2 Edition + Meetup in Bellabel Park game is out now and brings a bouquet of new experiences to the Flower Kingdom. Nintendo Switch 2 players will be able to check out Bellabel Park and visit the new a…</div>
+
+                                        <div className="h-auto w-11/12 mt-1">
+                                            <span className="border-b-2 border-red-600 pb-0.5 text-red-600 text-base tracking-wider font-semibold">Read More</span>
+                                        </div>
+
                                     </div>
+                        })}
+                    </div>
 
-                                    <div className="h-11 w-11/12 flex justify-start items-center">
-                                        <div className="mr-1 flex justify-center items-center"><i className='bx bx-plus-medical text-red-600 text-2xl' ></i></div>
-                                        <div className="text-gray-700 font-light text-sm">03/27/26</div>
-                                    </div>
+                    {/* new articles titles 2x2 */}
+                    <div className="h-auto w-full flex justify-center items-center">
 
-                                    <div className="h-auto w-11/12 text-gray-800 font-semibold text-base tracking-wide line-clamp-2">Get ready for some wondrous new content for Super Mario Bros. Wonder—exclusive to Nintendo Switch 2!</div>
+                        <div className="h-full w-11/12 flex flex-wrap justify-between">
+                            {Array.from({length: 4}).map((_, index) => {
+                                return <div key={index} className="h-auto w-40 mb-6">
 
-                                    <div className="h-auto w-11/12 mt-2 text-gray-800 line-clamp-2 text-sm font-light">The Super Mario Bros.™ Wonder – Nintendo Switch™ 2 Edition + Meetup in Bellabel Park game is out now and brings a bouquet of new experiences to the Flower Kingdom. Nintendo Switch 2 players will be able to check out Bellabel Park and visit the new a…</div>
+                                            <div className="border border-gray-200 h-24 w-full bg-red-600 rounded-lg"></div>
 
-                                    <div className="h-auto w-11/12">
-                                        <span className="border-b-2 border-red-600 pb-0.5 text-red-600 text-sm tracking-wider font-semibold">Read More</span>
-                                    </div>
+                                            <div className="h-auto w-full mt-2 flex">
+                                                <div className="flex justify-center items-center"><i className='bx bxs-square text-red-600 text-2xl' ></i></div>
+                                                <div className="h-auto w-auto ml-1 flex justify-center items-center text-gray-700 text-sm font-light">03/26/26</div>
+                                            </div>
 
-                                </div>
-                    })}
+                                            <div className="h-auto w-full mt-2 line-clamp-3 text-sm text-gray-700 font-bold">Here’s the latest Nintendo Switch Online news from March</div>
 
-                    {/* News Arrticle Small 2x2 */}
-                    <div className="border h-auto w-full flex justify-center items-center">
+                                            <div className="mt-2">
+                                                <span className="border-b-2 border-red-600 h-auto w-auto text-red-600 font-bold tracking-wide">Read More</span>
+                                            </div>
 
-                        <div className="border h-full w-11/12 flex flex-wrap justify-between">
-                            {Array.from({length: 2}).map((_, index) => {
-                                return <div key={index} className="border border-red-600 h-50 w-40">
-                                            <div className="border h-24 w-full rounded-lg"></div>
-                                            <div></div>
                                         </div>
                             })}
                         </div>
@@ -253,14 +267,21 @@ function DashboardPage(){
                     </div>
 
                     {/* See all news articles BUTTON */}
-                    <div className="border h-20 w-full">see all news articles</div>
+                    <div className="border-b border-gray-300 h-24 w-full flex justify-center items-start">
+                        <div className="h-12 w-11/12 mt-2 flex justify-center items-center bg-red-600 rounded-md">
+                            <div className="mr-4 flex justify-center items-center"><i className='bx bxs-message text-xl text-white' ></i></div>
+                            <div className="text-white text-base tracking-wider font-semibold">See all news articles</div>
+                        </div>
+                    </div>
+
+
                 </div>
 
                 {/* Characters Block */}
-                <div className="border-2 border-blue-600 h-auto w-full flex flex-wrap justify-around items-center">
+                <div className="h-auto w-full flex flex-wrap justify-around items-center">
 
                     {/* title */}
-                    <div className="border border-red-600 h-20 w-full">character title</div>
+                    <div className="h-20 w-full">character title</div>
 
                     {/* tiles */}
                     {Array.from({length: 8}).map((_, index) => {
