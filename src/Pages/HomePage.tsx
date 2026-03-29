45d6e6b70501dd5dd5d14c54ec7f4d176f13ed04
loganyeh <yehlogan@gmail.com>
2026-03-29 01:58:24 -0400

    feat: static ui for NavBar

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -26,6 +26,10 @@ export type CharactersData = {
    characters: string[];
 };
 
+type NavData = {
+    nav: string[];
+}
+
 function DashboardPage(){
     const redButtonBlockData: RedButtonBlockType[] = [
         {title: "Nintendo Today!", description: "Your movie companion: Nintendo Today!", buttonText: "Learn more", finePrint: "Nintendo Account, compatible smart device and persistent internet connection required. Data charges may apply. Requires iOS 16.0 or later. Requires Android 10.0 or later.", icon: "" },
@@ -33,10 +37,12 @@ function DashboardPage(){
         {title: "Online Store", description: "Shop games, exclusive Nintendo merchandise, and more! Plus, get free shipping on orders $50 and over.", buttonText: "Start shopping", finePrint: "", icon: ""},
         {title: "Nintendo Switch Online + Expansion Pack", description: "Play online, discover the classics, and more", buttonText: "Learn more", finePrint: "*Full version of game required to use the content for that game. Sold separately. For details visit https://support.nintendo.com/switch2/upgradepack. Terms apply. https://www.nintendo.com/us/purchase-terms/", icon: "" },
     ]
-
     const charactersSectionData: CharactersData = {
         characters: ["Super Mario", "The Legend of Zelda", "Splatoon", "Kirby", "Pikmin", "Animal Crossing", "Metroid", "Pokemon"],
     };
+    const navIcons: NavData = {
+        nav: ['bx bxs-coin-stack','bx bxs-heart', 'bx bx-search', 'bx bxs-cart', 'bx bxl-reddit']
+    }
 
     useEffect(() => {
         async function getPlatform(){
@@ -67,6 +73,27 @@ function DashboardPage(){
 
                 {/* Digital Best Sellers */}
                 <DigitalsSection />
+
+                <div className="fixed bottom-0 h-24 w-full flex justify-center items-center">
+
+                    <div className="border border-zinc-300 h-[60px] w-[375px] px-3 flex justify-around items-center bg-white shadow-2xl rounded-full">
+                        {/* {navIcons.nav.map((nav, index) => {
+                            return <div key={index} className="border h-full w-1/6 flex justify-center items-center">
+                                <i className={`${nav} text-3xl text-zinc-700`}></i>
+                            </div>
+                        })} */}
+                        {/* ['bx bxs-coin-stack','bx bxs-heart', 'bx bx-search', 'bx bxs-cart', 'bx bxl-reddit'] */}
+                        <div className="h-full w-1/6 flex justify-center items-center shrink-0"><i className={`bx bx-menu text-4xl text-zinc-700`}></i></div>
+                        <div className="h-full w-1/6 flex justify-center items-center shrink-0"><i className={`bx bxs-heart text-3xl text-zinc-700`}></i></div>
+
+                        <div className="h-16 w-16 mb-4 flex justify-center items-center bg-red-600 rounded-full shrink-0"><i className={`bx bx-search text-4xl text-white`}></i></div>
+
+                        <div className="h-full w-1/6 flex justify-center items-center shrink-0"><i className={`bx bxs-cart text-3xl text-zinc-700`}></i></div>
+                        <div className="h-full w-1/6 flex justify-center items-center shrink-0"><i className={`bx bxs-user text-3xl text-zinc-700`}></i></div>
+
+                    </div>
+
+                </div>
 
                 <Footer />
             </div>
