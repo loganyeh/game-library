2b282a74251b941678f48d971beffb45d5e017cc
loganyeh <yehlogan@gmail.com>
2026-03-30 16:31:37 -0400

    wip: Showcase img 1 loaded

--- src/API/HomepageAPI.ts
+++ src/API/HomepageAPI.ts
@@ -1,5 +1,21 @@
 
-
-export async function fetchGame(){
-
+// api SHOWCASE
+export type Game = {
+    id: number;
+    name: string;
+    background_image: string;
 }
+type ShowcaseApi = {
+    results: Game[];
+}
+
+export async function fetchShowcase(){
+    const response = await fetch(`https://api.rawg.io/api/games?key=59f33c3c1f1b42118f8f6fcd1cbb7e80`);
+    const data: ShowcaseApi = await response.json();
+
+    console.log(data.results);
+
+    return data.results;
+}
+
+fetchShowcase();
--- src/Components/HomePageComponents/ShowcaseSlideshow.tsx
+++ src/Components/HomePageComponents/ShowcaseSlideshow.tsx
@@ -1,17 +1,53 @@
+import { useState, useEffect } from "react";
+import { fetchShowcase } from "../../API/HomepageAPI";
 
+import type { Game } from "../../API/HomepageAPI";
 
 function ShowcaseSlideshow(){
+    const [loading, setLoading] = useState(true);
+    const [showcaseData, setShowcaseData] = useState<Game[]>([]);
+
+    useEffect(() => {
+        async function getShowcase(){
+            setLoading(true);
+
+            const data = await fetchShowcase();
+            setShowcaseData(data);
+
+            setLoading(false);
+        }
+
+        getShowcase();
+    }, []);
+
+    console.log(showcaseData.slice(0, 10));
 
     return(
         <>
             <div className="h-auto w-full">
                 {/* Image Slideshow */}
-                <div className="h-[425px] w-full bg-blue-300"></div>
+                <div className="h-[425px] w-full bg-blue-300">
+                    {loading ?
+                        (<div className="h-full flex justify-center">
+                            <div className="flex justify-center items-center animate-spin"><i className='bx bx-loader-alt text-4xl'></i></div>
+                        </div>) 
+                        :
+                        (showcaseData.slice(8, 9).map((game, _) => {
+                            return <div key={game.id} className={`h-full w-full bg-center bg-cover`} style={{backgroundImage: `url(${game.background_image})`}}>
+                                
+                            </div>
+                        }))
+                    }
+                    
+                </div>
 
                 {/* Slideshow Preview */}
                 <div className="h-20 w-full flex flex-col justify-center items-center">
                     <div className="h-full w-9/12 flex justify-between items-center">
                         <div className="h-8 w-8 flex justify-center items-center bg-red-600 rounded-full"><i className='bx bx-pause text-white text-4xl'></i></div>
+                        {/* {Array.from({length: 4}).map((_, index) => {
+                            return <div key={index} className="h-12 w-12 bg-blue-300 rounded-lg"></div>
+                        })} */}
                         {Array.from({length: 4}).map((_, index) => {
                             return <div key={index} className="h-12 w-12 bg-blue-300 rounded-lg"></div>
                         })}
