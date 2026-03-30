deb675cd61ba1b21f65117fb92d0ed8c437b32df
loganyeh <yehlogan@gmail.com>
2026-03-30 16:56:09 -0400

    feat: loaded images for slideshow. wip: showcase sliding

--- src/Components/HomePageComponents/ShowcaseSlideshow.tsx
+++ src/Components/HomePageComponents/ShowcaseSlideshow.tsx
@@ -20,37 +20,45 @@ function ShowcaseSlideshow(){
         getShowcase();
     }, []);
 
-    console.log(showcaseData.slice(0, 10));
+    // console.log(showcaseData.slice(0, 10));
+    console.log(showcaseData[8]);
 
     return(
         <>
             <div className="h-auto w-full">
                 {/* Image Slideshow */}
-                <div className="h-[425px] w-full bg-blue-300">
+                <div className="h-[425px] w-full flex justify-center items-center bg-gray-300">
                     {loading ?
-                        (<div className="h-full flex justify-center">
-                            <div className="flex justify-center items-center animate-spin"><i className='bx bx-loader-alt text-4xl'></i></div>
-                        </div>) 
+                        (<div className="flex justify-center items-center animate-spin"><i className='bx bx-loader-alt text-4xl'></i></div>)
                         :
                         (showcaseData.slice(8, 9).map((game, _) => {
                             return <div key={game.id} className={`h-full w-full bg-center bg-cover`} style={{backgroundImage: `url(${game.background_image})`}}>
                                 
                             </div>
                         }))
+                        // (
+                        //     <div className={`h-full w-full bg-center bg-cover`} style={{backgroundImage: `url(${showcaseData[0].background_image})`}}>
+                                
+                        //     </div>
+                        // )
                     }
-                    
                 </div>
 
                 {/* Slideshow Preview */}
                 <div className="h-20 w-full flex flex-col justify-center items-center">
                     <div className="h-full w-9/12 flex justify-between items-center">
                         <div className="h-8 w-8 flex justify-center items-center bg-red-600 rounded-full"><i className='bx bx-pause text-white text-4xl'></i></div>
-                        {/* {Array.from({length: 4}).map((_, index) => {
-                            return <div key={index} className="h-12 w-12 bg-blue-300 rounded-lg"></div>
-                        })} */}
-                        {Array.from({length: 4}).map((_, index) => {
-                            return <div key={index} className="h-12 w-12 bg-blue-300 rounded-lg"></div>
-                        })}
+                        {loading ?
+                            (Array.from({length: 4}).map((_, index) => {
+                                return <div key={index} className="h-12 w-12 flex justify-center items-center bg-gray-300 rounded-lg">
+                                    <div className="flex justify-center items-center animate-spin"><i className='bx bx-loader-alt'></i></div>
+                                </div>
+                            })) 
+                            :
+                            (showcaseData.slice(0, 4).map((game, index) => {
+                                return <div key={index} className="h-12 w-12 bg-gray-300 rounded-lg bg-center bg-cover" style={{backgroundImage: `url(${game.background_image})`}}></div>
+                            }))
+                        }
                     </div>
                     <div className="border-b border-gray-300 h-auto w-11/12"></div>
                 </div>
