22cae25fc3b2fa526d503de87668d1e45dda0e4f
loganyeh <yehlogan@gmail.com>
2026-03-30 17:37:21 -0400

    feat: FeaturedSection imgs

--- src/API/HomepageAPI.ts
+++ src/API/HomepageAPI.ts
@@ -8,14 +8,32 @@ export type Game = {
 type ShowcaseApi = {
     results: Game[];
 }
-
 export async function fetchShowcase(){
     const response = await fetch(`https://api.rawg.io/api/games?key=59f33c3c1f1b42118f8f6fcd1cbb7e80`);
     const data: ShowcaseApi = await response.json();
+    // console.log(data.results);
 
+    return data.results;
+}
+
+// api FEATURED
+export type FeatureDM = {
+    id: number;
+    name: string;
+    background_image: string;
+}
+type FeatureApi = {
+    results: FeatureDM[];
+}
+
+export async function fetchFeature(){
+    const response = await fetch(`https://api.rawg.io/api/games?key=59f33c3c1f1b42118f8f6fcd1cbb7e80`);
+    const data: FeatureApi = await response.json();
     console.log(data.results);
 
     return data.results;
 }
 
-fetchShowcase();
+
+
+
--- src/Components/HomePageComponents/FeaturedSection.tsx
+++ src/Components/HomePageComponents/FeaturedSection.tsx
@@ -1,22 +1,49 @@
+import { useState, useEffect } from "react";
+import SectionTitle from "./HomepageSharedComps/SectionTitle";
 import FeatureTile from "./FeaturedComps/FeatureTile";
-
+import { fetchFeature } from "../../API/HomepageAPI";
+import type { FeatureDM } from "../../API/HomepageAPI";
 
 function FeaturedSection(){
+    const [loading, setLoading] = useState(true);
+    
+    const [featureData, setFeatureData] = useState<FeatureDM[]>([]);
+
+    useEffect(() => {
+        async function getFeature(){
+            setLoading(true);
+
+            const data = await fetchFeature();
+            setFeatureData(data);
+
+            setLoading(false);
+        }
+
+        getFeature();
+    }, []);
 
     return(
         <>
             <div className="border-b border-gray-300 h-auto w-full pb-6">     
                 {/* title */}
-                <div className="h-16 w-full flex justify-start items-end">
-                    <span className="ml-4 mb-2 tracking-wider text-xl text-gray-600 font-bold">Featured</span>
-                </div>
+                <SectionTitle title="Featured"/>
 
                 {/* Featured Scroll Tiles */}
-                <div className="h-80 w-auto flex justify-start items-center overflow-x-auto scroll-smooth">
-
-                    {Array.from({length: 10}).map((_, index) => {
-                        return <FeatureTile key={index} />
-                    })}
+                <div className="h-auto w-auto py-1 flex justify-start items-center overflow-x-auto scroll-smooth">
+                    {loading ?
+                        (Array.from({length: 10}).map((_, index) => {
+                            return <div key={index} className="border border-gray-300 h-64 w-52 mx-4 rounded-lg shrink-0">
+                                        <div className="h-28 w-full flex justify-center items-end bg-gray-300 rounded-t-lg">
+                                            <div className="flex justify-center items-center -mb-3 animate-spin"><i className='bx bx-loader-alt text-2xl'></i></div>
+                                        </div>
+                                        <div className="h-36 w-full flex flex-col justify-between items-center rounded-b-lg"></div>
+                                    </div>
+                        }))
+                        :
+                        (featureData.slice(0, 10).map((game, index) => {
+                            return <FeatureTile key={index} name={game.name} background_image={game.background_image} />
+                        }))
+                    }
 
                 </div>
 
