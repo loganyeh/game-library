ae2778fcd1727752308a6d7395264fe279ef100c
loganyeh <yehlogan@gmail.com>
2026-03-30 22:38:52 -0400

    feat: DigitalsSection, Footer @ breakpoints MD. BREAKPOINT MD Homepage DONE

--- src/Components/HomePageComponents/DigitalsSection.tsx
+++ src/Components/HomePageComponents/DigitalsSection.tsx
@@ -7,21 +7,21 @@ function DigitalsSection(){
         <>
             {digitalSectionTitles.map((title, index) => {
                 return <div key={index} className="h-auto w-full">
-                            <div className="h-auto w-full flex flex-col justify-start items-center">
+                            <div className="h-auto w-full flex flex-col md:flex-row justify-start items-center">
                                 <SectionTitle title={`Digital ${title}`} />
-                                <div className="h-auto w-full">
-                                    <span className="border-b-3 border-red-600 h-full ml-4 font-semibold text-red-600 text-sm tracking-wide">See full list</span>
+                                <div className="md:border-l-2 md:border-gray-300 h-auto w-full -ml-50 mt-4">
+                                    <span className="border-b-3 border-red-600 h-full ml-4 font-semibold text-red-600 text-sm md:text-base tracking-wide">See full list</span>
                                 </div>
                             </div>
                             
                             <div className="h-auto w-auto mt-4 pl-5 pb-8 flex overflow-x-auto scroll-smooth">
                                 {Array.from({length: 10}).map((_, index) => {
-                                    return <div key={index} className="border border-gray-600 min-h-96 w-56 mx-4 rounded-lg shrink-0">
+                                    return <div key={index} className="border border-gray-600 min-h-96 w-56 md:w-[270px] mx-4 rounded-lg shrink-0">
                                                 <div className="h-5 w-full bg-red-600 flex justify-center items-center text-white font-bold rounded-t-lg">2</div>
 
-                                                <div className="h-52 w-full bg-blue-300"></div>
+                                                <div className="h-52 md:h-[255px] w-full bg-blue-300"></div>
 
-                                                <div className="h-auto w-full flex flex-col justify-center items-center">
+                                                <div className="h-auto md:h-52 w-full flex flex-col justify-center md:justify-around items-center">
                                                     <div className="h-auto w-11/12 mt-3 flex justify-start items-center font-semibold text-base text-gray-700 line-clamp-2">Super Mario Bros. Wonder - Nintendo Switch 2</div>
                                                     <div className="h-auto w-11/12 font-light text-[11px] text-gray-600">3/26/26</div>
 
@@ -45,7 +45,7 @@ function DigitalsSection(){
                                 })}
 
                                 <div className="h-auto w-40 flex items-end shrink-0">
-                                    <div className="h-[364px] w-full flex justify-center items-center bg-red-600 text-white font-semibold text-base tracking-wide rounded-md">See full list</div>
+                                    <div className="h-[364px] md:h-[465px] w-full flex justify-center items-center bg-red-600 text-white font-semibold text-base tracking-wide rounded-md">See full list</div>
                                 </div>
 
                             </div>
