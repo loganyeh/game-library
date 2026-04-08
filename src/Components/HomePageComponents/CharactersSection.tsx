c1a04b03b14afa6449f3c8b3d803fb07100ca54d
loganyeh <yehlogan@gmail.com>
2026-04-07 23:08:28 -0400

    update: CharactersSection breakpoints & cleanup

--- src/Components/HomePageComponents/CharactersSection.tsx
+++ src/Components/HomePageComponents/CharactersSection.tsx
@@ -10,22 +10,22 @@ function CharactersSection({ characters }: CharacterProp){
 
     return(
         <>
-            <div className="mx-4 lg:mx-6 flex flex-col">
+            <div className="mx-4 lg:mx-6 flex flex-col items-center">
 
                 {/* title */}
                 <SectionTitle title="Characters" />
 
                 {/* tiles */}
-                <div className="w-full max-w-6xl grid grid-cols-2 gap-4">
+                <div className="w-full max-w-6xl grid grid-cols-2 gap-4 md:grid-cols-4 2xl:gap-5">
                     {characters.characters.map((character, index) => {
                         return <div key={index} className="flex flex-col gap-2">
                                     <div className="aspect-square bg-blue-300 rounded-lg"></div>
-                                    <div className="text-lg md:text-xl font-bold text-zinc-600 break-word">{character || "The Legend of Zelda"}</div>
+                                    <div className="text-lg md:text-xl font-bold text-zinc-700 break-word">{character || "The Legend of Zelda"}</div>
                                 </div>
                     })}
                 </div>
 
-                <div className="border-b border-gray-300 w-full mt-8"></div>
+                <div className="border-b border-gray-300 w-full mt-8 md:mt-20 lg:mt-14"></div>
             </div>
         
         </>
